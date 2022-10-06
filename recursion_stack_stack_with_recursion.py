6d2b26ab08ac459b17a1145c5a7b2393692316f2
Patatatat <andres.guluarte@gmail.com>
2022-10-05 22:44:55 -0600

    combinig it to one file

--- recursion_stack_stack_with_recursion.py
+++ recursion_stack_stack_with_recursion.py
@@ -20,12 +20,30 @@
 #    elif item.is_a_key():
 #      print ('found the key!')
 
-def countdown(i):
-  print(i)
-  if i <= 1:
-    #print("happy new year!")
-    return
-  else:
-    countdown(i-1)
+# def countdown(i):
+#  print(i)
+#  if i <= 1:
+#    #print("happy new year!")
+#    return
+#  else:
+#    countdown(i-1)
 
-countdown(10)
+#countdown(10)
+
+#Stack:
+
+# def greet2(name):
+  # print ("how are you, " + name + "?")
+
+# def bye():
+  # print("ok bye!")
+
+# def greet(name):
+  # print("hello, " + name + "!")
+  # greet2(name)
+  # print("getting ready to say bye...")
+  # bye()
+
+#greet(name="Andres")
+
+# Stack with recursion:
